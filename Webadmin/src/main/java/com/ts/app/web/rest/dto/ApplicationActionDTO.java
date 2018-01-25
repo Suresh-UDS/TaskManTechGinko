75da4645820e5d7a8e88cc98de89fe25b7844ece
Gnanaprakash <gnanaprakash.r@uds.in>
2018-01-26 01:18:18 +0530

    changes for module action page

--- Webadmin/src/main/java/com/ts/app/web/rest/dto/ApplicationActionDTO.java
+++ Webadmin/src/main/java/com/ts/app/web/rest/dto/ApplicationActionDTO.java
@@ -1,22 +1,5 @@
 package com.ts.app.web.rest.dto;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import com.ts.app.domain.AbstractAuditingEntity;
-
-@Entity
-@Table(name = "application_action")
 public class ApplicationActionDTO extends BaseDTO {
 
 	/**
@@ -24,27 +7,9 @@ public class ApplicationActionDTO extends BaseDTO {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue
-	@Column(name = "id")
 	private long id;
 
-	@Column(name = "action_name")
 	private String name;
-
-	@ManyToMany(mappedBy = "moduleActions")
-	private Set<ApplicationModuleDTO> module;
-
-	@OneToMany(mappedBy = "action", cascade = CascadeType.ALL)
-	private Set<UserRolePermissionDTO> rolePermission;
-
-	public Set<UserRolePermissionDTO> getRolePermission() {
-		return rolePermission;
-	}
-
-	public void setRolePermission(Set<UserRolePermissionDTO> rolePermission) {
-		this.rolePermission = rolePermission;
-	}
 
 	public long getId() {
 		return id;
@@ -60,17 +25,6 @@ public class ApplicationActionDTO extends BaseDTO {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public Set<ApplicationModuleDTO> getModule() {
-		if (module == null) {
-			module = new HashSet<>();
-		}
-		return module;
-	}
-
-	public void setModule(Set<ApplicationModuleDTO> module) {
-		this.module = module;
 	}
 
 }
---
+++ Webadmin/src/main/webapp/scripts/app/admin/user-roles-permission/module-actions.controller.js
@@ -0,0 +1,360 @@
+'use strict';
+
+angular.module('timeSheetApp')
+    .controller('ModuleActionsController', function ($rootScope, $scope, $state, $timeout, ModuleActionComponent,EmployeeComponent, $http, $stateParams, $location, JobComponent) {
+        $scope.success = null;
+        $scope.error = null;
+        $scope.doNotMatch = null;
+        $scope.errorUserExists = null;
+        $scope.validationError = null;
+        $scope.validationErrorMsg = null;
+        $scope.authorities = ["User", "Admin"];
+
+        $timeout(function (){angular.element('[ng-model="name"]').focus();});
+
+        $scope.pages = { currPage : 1};
+
+        $scope.selectedGroup;
+
+        $scope.users;
+
+        $scope.moduleActions;
+
+        $scope.selectedModuleAction;
+        
+        $scope.moduleActions;
+        
+        $scope.moduleName;
+        
+        $scope.selectedActions=[];
+        
+        $scope.addAction = function() {
+        	var action = $scope.actionSelector
+        	console.log('action selected -' + action);
+        	$scope.selectedActions.push(action);
+        	console.log('action selected -' + action + ',' + $scope.selectedActions)
+        }
+        
+        $scope.removeAction = function(ind) {
+        	$scope.selectedActions.splice(ind,1);
+        }
+
+        $scope.saveModuleActions = function () {
+        	console.log('moduleActions -'+ $scope.moduleName);
+        	console.log('moduleActions -'+ $scope.selectedActions);
+        	var actions = []
+        	for(var i in $scope.selectedActions) {
+        		actions[i] = {
+        			"name" : $scope.selectedActions[i]		
+        		}
+        	}
+        	$scope.moduleActions = {
+        		"name" : $scope.moduleName,
+        		"moduleActions" : actions 
+        	}
+        	ModuleActionComponent.createModuleAction($scope.moduleActions).then(function () {
+            	$scope.success = 'OK';
+            	$scope.loadModuleActions();
+            	$location.path('/app-module-actions');
+            }).catch(function (response) {
+                $scope.success = null;
+                console.log(response.data);
+                if (response.status === 400 && response.data.message === 'error.duplicateRecordError') {
+                    $scope.errorModuleActionExists = true;
+                } else if(response.status === 400 && response.data.message === 'error.validation'){
+                	$scope.validationError = true;
+                	$scope.validationErrorMsg = response.data.description;
+                } else {
+                    $scope.error = 'ERROR';
+                }
+            });
+        };
+
+        $scope.cancelModuleAction = function () {
+        	$scope.selectedActions = [];
+        };
+
+        $scope.loadModuleActions = function () {
+
+        	$scope.search();
+        };
+
+        $scope.refreshPage = function() {
+        	$scope.clearFilter();
+        	$scope.loadUsers();
+        }
+
+
+
+        $scope.loadModuleAction = function() {
+        	console.log('loadModuleAction -' + $stateParams.id);
+        	ModuleActionComponent.findOne($stateParams.id).then(function (data) {
+                $scope.moduleAction = data;
+            });
+
+        };
+
+        $scope.updateModuleAction = function () {
+        	console.log('ModuleAction details - ' + JSON.stringify($scope.moduleAction));
+
+        	ModuleActionComponent.updateModuleAction($scope.moduleAction).then(function () {
+            	$scope.success = 'OK';
+            	$scope.loadModuleActions();
+            	$location.path('/user-roles');
+            }).catch(function (response) {
+                $scope.success = null;
+                console.log('Error - '+ response.data);
+                if (response.status === 400 && response.data.message === 'error.duplicateRecordError') {
+                    $scope.errorModuleActionExists = true;
+                } else if(response.status === 400 && response.data.message === 'error.validation'){
+                	$scope.validationError = true;
+                	$scope.validationErrorMsg = response.data.description;
+                } else {
+                    $scope.error = 'ERROR';
+                }
+            });
+        };
+
+        $scope.deleteConfirm = function (user){
+        	console.log('...>>>delete confirm<<<');
+        	$scope.confirmModuleAction = moduleAction;
+        	console.log(moduleAction.id);
+        }
+
+        $scope.deleteModuleAction = function () {
+        	console.log("user>>>>",+$scope.confirmModuleAction);
+//        	$scope.user = user;
+        	ModuleActionComponent.deleteModuleAction($scope.confirmModuleAction);
+        	$scope.success = 'OK';
+        	$state.reload();
+        };
+
+        $scope.search = function () {
+        	var currPageVal = ($scope.pages ? $scope.pages.currPage : 1);
+        	if(!$scope.searchCriteria) {
+            	var searchCriteria = {
+            			currPage : currPageVal
+            	}
+            	$scope.searchCriteria = searchCriteria;
+        	}
+
+        	$scope.searchCriteria.currPage = currPageVal;
+        	console.log('Selected  module action -' + $scope.selectedModuleAction);
+
+        	if(!$scope.selectedModuleAction) {
+        		if($rootScope.searchCriteriaModuleAction) {
+            		$scope.searchCriteria = $rootScope.searchCriteriaModuleAction;
+        		}else {
+        			$scope.searchCriteria.findAll = true;
+        		}
+
+        	}else {
+	        	if($scope.selectedModuleAction) {
+	        		$scope.searchCriteria.findAll = false;
+		        	$scope.searchCriteria.moduleActionId = $scope.selectedModuleAction.id;
+		        	$scope.searchCriteria.name = $scope.selectedModuleAction.name;
+		        	$scope.searchCriteria.activeFlag = $scope.selectedModuleAction.activeFlag;
+		        	console.log('selected user role id ='+ $scope.searchCriteria.moduleActionId);
+	        	}else {
+	        		$scope.searchCriteria.moduleActionId = 0;
+	        	}
+        	}
+        	console.log($scope.searchCriteria);
+        	ModuleActionComponent.search($scope.searchCriteria).then(function (data) {
+                $scope.moduleActions = data.transactions;
+                console.log($scope.moduleActions);
+                $scope.pages.currPage = data.currPage;
+                $scope.pages.totalPages = data.totalPages;
+                if($scope.moduleActions == null){
+                    $scope.pages.startInd = 0;
+                }else{
+                    $scope.pages.startInd = (data.currPage - 1) * 10 + 1;
+                }
+
+                $scope.pages.endInd = data.totalCount > 10  ? (data.currPage) * 10 : data.totalCount ;
+                $scope.pages.totalCnt = data.totalCount;
+            	$scope.hide = true;
+            });
+        	$rootScope.searchCriteriaModuleAction = $scope.searchCriteria;
+        	if($scope.pages.currPage == 1) {
+            	$scope.firstStyle();
+        	}
+        };
+
+
+        $scope.first = function() {
+            if($scope.pages.currPage > 1) {
+                $scope.pages.currPage = 1;
+                $scope.firstStyle();
+                $scope.search();
+            }
+        };
+
+        $scope.firstStyle = function() {
+            var first = document.getElementById('#first');
+            var ele = angular.element(first);
+            ele.addClass('disabledLink');
+            var previous = document.getElementById('#previous');
+            ele = angular.element(previous);
+            ele.addClass('disabledLink');
+            if($scope.pages.totalPages > 1) {
+                var nextSitePage = document.getElementById('#next');
+                var ele = angular.element(next);
+                ele.removeClass('disabledLink');
+                var lastSitePage = document.getElementById('#lastSitePage');
+                ele = angular.element(lastSitePage);
+                ele.removeClass('disabledLink');
+            }
+
+        }
+
+        $scope.previous = function() {
+            console.log("Calling previous")
+
+            if($scope.pages.currPage > 1) {
+                $scope.pages.currPage = $scope.pages.currPage - 1;
+                if($scope.pages.currPage == 1) {
+                    var first = document.getElementById('#first');
+                    var ele = angular.element(first);
+                    ele.addClass('disabled');
+                    var previous = document.getElementById('#previous');
+                    ele = angular.element(previous);
+                    ele.addClass('disabled');
+                }
+                var next = document.getElementById('#next');
+                var ele = angular.element(next);
+                ele.removeClass('disabled');
+                var lastSitePage = document.getElementById('#last');
+                ele = angular.element(last);
+                ele.removeClass('disabled');
+                $scope.search();
+            }
+
+        };
+
+        $scope.next = function() {
+            console.log("Calling next")
+
+            if($scope.pages.currPage < $scope.pages.totalPages) {
+                $scope.pages.currPage = $scope.pages.currPage + 1;
+                if($scope.pages.currPage == $scope.pages.totalPages) {
+                    var next = document.getElementById('#next');
+                    var ele = angular.element(next);
+                    ele.addClass('disabled');
+                    var last = document.getElementById('#last');
+                    ele = angular.element(last);
+                    ele.addClass('disabled');
+                }
+                var first = document.getElementById('#first')
+                var ele = angular.element(first);
+                ele.removeClass('disabled');
+                var previous = document.getElementById('#previous')
+                ele = angular.element(previous);
+                ele.removeClass('disabled');
+                $scope.search();
+            }
+
+        };
+
+        $scope.last = function() {
+            console.log("Calling last")
+            if($scope.pages.currPage < $scope.pages.totalPages) {
+                $scope.pages.currPage = $scope.pages.totalPages;
+                if($scope.pages.currPage == $scope.pages.totalPages) {
+                    var next = document.getElementById('#next');
+                    var ele = angular.element(next);
+                    ele.addClass('disabled');
+                    var last = document.getElementById('#last');
+                    ele = angular.element(last);
+                    ele.addClass('disabled');
+                }
+                var first = document.getElementById('#first');
+                var ele = angular.element(first);
+                ele.removeClass('disabled');
+                var previous = document.getElementById('#previous');
+                ele = angular.element(previous);
+                ele.removeClass('disabled');
+                $scope.search();
+            }
+
+        };
+
+        $scope.clearFilter = function() {
+            $scope.selectedSite = null;
+            $scope.selectedProject = null;
+            $scope.searchCriteria = {};
+            $rootScope.searchCriteriaSite = null;
+            $scope.pages = {
+                currPage: 1,
+                totalPages: 0
+            }
+            $scope.search();
+        };
+
+
+            $scope.inputType = "password";
+            $scope.passwordCheckbox = "true";
+        $scope.showPwd = function(){
+              if($scope.inputType == "password"){
+                    $scope.inputType = "text";
+                    $scope.passwordCheckbox = "false";
+
+              }else{
+                $scope.inputType = "password";
+                    $scope.passwordCheckbox = "true";
+
+              }
+
+        }
+
+        // Datatable
+                        $scope.initDataTables = function(){
+
+                            console.log("Data tables function")
+
+                            $('#datatables').DataTable({
+                                "pagingType": "full_numbers",
+                                "lengthMenu": [
+                                    [10, 25, 50, -1],
+                                    [10, 25, 50, "All"]
+                                ],
+                                responsive: true,
+                                language: {
+                                    search: "_INPUT_",
+                                    searchPlaceholder: "Search records",
+                                }
+
+                            });
+
+
+                            var table = $('#datatables').DataTable();
+
+                            // Edit record
+                            table.on('click', '.edit', function() {
+                                $tr = $(this).closest('tr');
+
+                                var data = table.row($tr).data();
+                                alert('You press on Row: ' + data[0] + ' ' + data[1] + ' ' + data[2] + '\'s row.');
+                            });
+
+                            // Delete a record
+                            table.on('click', '.remove', function(e) {
+                                $tr = $(this).closest('tr');
+                                table.row($tr).remove().draw();
+                                e.preventDefault();
+                            });
+
+                            //Like record
+                            table.on('click', '.like', function() {
+                                alert('You clicked on Like button');
+                            });
+
+                            $('.card .material-datatables label').addClass('form-group');
+
+                        }
+
+
+
+    });
+
+
