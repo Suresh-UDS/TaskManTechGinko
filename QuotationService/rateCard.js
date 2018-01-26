41ad939b560020c42f792a03e3495cac90826b74
Karthick <karthickkumararajak@gmail.com>
2018-01-26 11:06:48 +0530

    rate card service node

---
+++ QuotationService/rateCard.js
@@ -0,0 +1,22 @@
+// import the necessary modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// model creation
+
+var RateCardModel = function() {
+    var RateCardSchema = new Schema({
+        title: String,
+        type: String,
+        cost: Number,
+        uom: String,
+        lastModifiedDate: Date
+    });
+
+
+    // register the mongoose model
+    mongoose.model('RateCard', RateCardSchema);
+};
+
+// create an export function to encapsulate the model creation
+module.exports = RateCardModel;
--- Webadmin/src/main/webapp/scripts/app/admin/quotation/rateCard.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/quotation/rateCard.controller.js
@@ -1,8 +1,15 @@
 'use strict';
 
 angular.module('timeSheetApp')
-    .controller('RateCardController', function ($scope, $rootScope, $state, $timeout,$http,$stateParams,$location) {
+    .controller('RateCardController', function ($scope, $rootScope, $state, $timeout,$http,$stateParams,$location, RateCardComponent    ) {
 
+    	$scope.rateCard;
 
+    	$scope.saveRateCard = function(rateCardDetails){
+    		console.log(rateCardDetails);
+            RateCardComponent.createRateCard(rateCardDetails).then(function (response) {
+                console.log(response);
+            })
+    	}
 
     });
