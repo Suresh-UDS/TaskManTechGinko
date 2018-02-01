a95924799eb89fca80499b5d21a6798861b2941a
Ushamurugesan <ushamurugesan18@gmail.com>
2018-02-01 11:24:35 +0530

    create employee

---
+++ MobileApp/src/pages/employee-list/create-employee.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {IonicPage, LoadingController, NavController, NavParams, ToastController} from 'ionic-angular';
+import {AttendanceListPage} from "../attendance-list/attendance-list";
+import {authService} from "../service/authService";
+import {Camera, CameraOptions} from "@ionic-native/camera";
+import {Geolocation} from "@ionic-native/geolocation";
+import {Geofence} from "@ionic-native/geofence";
+import {componentService} from "../service/componentService";
+import {EmployeeDetailPage} from "./employee-detail";
+
+/**
+ * Generated class for the EmployeeList page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-create-employee',
+  templateUrl: 'create-employee.html',
+})
+export class CreateEmployeePage {
+
+  employee:any;
+  firstLetter:any;
+  categories:any;
+  constructor(public navCtrl: NavController,public component:componentService,public myService:authService, public navParams: NavParams, private  authService: authService, public camera: Camera,
+              private loadingCtrl:LoadingController, private geolocation:Geolocation, private toastCtrl:ToastController,
+              private geoFence:Geofence) {
+
+    this.categories = 'basic';
+  }
+
+  ionViewDidLoad()
+  {
+
+  }
+}
