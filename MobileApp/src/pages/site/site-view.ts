1fd4620884a59516049595ab38764e5920929a65
Karthick <karthickkumararajak@gmail.com>
2018-02-05 12:43:21 +0530

    site maps removed

--- MobileApp/src/pages/site/site-view.ts
+++ MobileApp/src/pages/site/site-view.ts
@@ -2,15 +2,6 @@ import { Component } from '@angular/core';
 import {NavController, NavParams, ToastController} from 'ionic-angular';
 import {authService} from "../service/authService";
 import {componentService} from "../service/componentService";
-
-import {
-    GoogleMaps,
-    GoogleMap,
-    GoogleMapsEvent,
-    Marker,
-    GoogleMapsAnimation,
-    MyLocation
-} from '@ionic-native/google-maps';
 import {JobService} from "../service/jobService";
 import {AttendanceService} from "../service/attendanceService";
 
@@ -20,8 +11,6 @@ import {AttendanceService} from "../service/attendanceService";
 })
 export class SiteViewPage {
 
-    mapReady: boolean = false;
-    map: GoogleMap;
   siteName:any;
   siteDetail:any;
   categories:any;
@@ -30,7 +19,7 @@ export class SiteViewPage {
   ref=false;
   job="job";
   attendance="attendance";
-  constructor(public navCtrl: NavController,public component:componentService,public navParams:NavParams,public myService:authService,public authService:authService, private googleMaps: GoogleMaps, public toastCtrl: ToastController,
+  constructor(public navCtrl: NavController,public component:componentService,public navParams:NavParams,public myService:authService,public authService:authService, public toastCtrl: ToastController,
 
               private jobService:JobService, private attendanceService: AttendanceService) {
   this.categories='detail';
@@ -41,67 +30,8 @@ export class SiteViewPage {
 
   ionViewDidLoad()
   {
-      this.loadMap(this.siteDetail.name,this.siteDetail.addressLat,this.siteDetail.addressLng);
-      // this.loadMap();
   }
 
-    loadMap(siteName,lat,lng) {
-        // Create a map after the view is loaded.
-        // (platform is already ready in app.component.ts)
-
-        this.map = GoogleMaps.create('map_canvas', {
-            camera: {
-                target: {
-                    lat: lat,
-                    lng: lng
-                },
-                zoom: 18,
-                tilt: 30
-            }
-        });
-
-        // Wait the maps plugin is ready until the MAP_READY event
-        this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-            this.mapReady = true;
-        });
-    }
-
-    onButtonClick() {
-        if (!this.mapReady) {
-            this.showToast('map is not ready yet. Please try again.');
-            return;
-        }
-        this.map.clear();
-
-        // Get the location of you
-        this.map.getMyLocation()
-            .then((location: MyLocation) => {
-                console.log(JSON.stringify(location, null ,2));
-
-                // Move the map camera to the location with animation
-                return this.map.animateCamera({
-                    target: location.latLng,
-                    zoom: 17,
-                    tilt: 30
-                }).then(() => {
-                    // add a marker
-                    return this.map.addMarker({
-                        title: '@ionic-native/google-maps plugin!',
-                        snippet: 'This plugin is awesome!',
-                        position: location.latLng,
-                        animation: GoogleMapsAnimation.BOUNCE
-                    });
-                })
-            }).then((marker: Marker) => {
-            // show the infoWindow
-            marker.showInfoWindow();
-
-            // If clicked it, display the alert
-            marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-                this.showToast('clicked!');
-            });
-        });
-    }
 
     showToast(message: string) {
         let toast = this.toastCtrl.create({
