ff3afd2ffd7897646dcd9f5bad9ab14c52b53356
Praveen Sekar <praveens@techginko.com>
2018-03-03 19:20:47 +0530

    feedback changes

---
+++ Webadmin/src/main/webapp/scripts/app/admin/feedback/feedback-setup.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('timeSheetApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('feedback-setup', {
+                parent: 'manage',
+                url: '/feedback-setup',
+                controller: 'FeedbackSetupController',
+                data: {
+                    authorities: [],
+                    pageTitle: 'Feedback Setup'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/admin/feedback/feedback-setup.html',
+                        controller: 'FeedbackSetupController'
+                    }
+                },
+                resolve: {
+
+                }
+            })
+            .state('add-feedback-setup', {
+                parent: 'manage',
+                url: '/add-feedback-setup',
+                controller: 'FeedbackSetupController',
+                data: {
+                    authorities: [],
+                    pageTitle: 'Add Feedback Setup'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/admin/feedback/add-feedback-setup.html',
+                        controller: 'FeedbackSetupController'
+                    }
+                },
+                resolve: {
+
+                }
+            })
+    });
