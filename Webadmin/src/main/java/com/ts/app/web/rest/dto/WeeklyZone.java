6933f2aee39f68c4573d6d41b1e617924fa6b4d7
Gnanaprakash <gnanaprakash.r@uds.in>
2018-03-21 14:19:37 +0530

    added date field to weekly zone

--- Webadmin/src/main/java/com/ts/app/web/rest/dto/WeeklyZone.java
+++ Webadmin/src/main/java/com/ts/app/web/rest/dto/WeeklyZone.java
@@ -1,9 +1,13 @@
 package com.ts.app.web.rest.dto;
 
+import java.util.Date;
+
 public class WeeklyZone {
 	
 	private double rating;
 	private Long day;
+	
+	private Date date;
 	
 	public double getRating() {
 		return rating;
@@ -16,6 +20,12 @@ public class WeeklyZone {
 	}
 	public void setDay(Long day) {
 		this.day = day;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
 	}
 	
 
