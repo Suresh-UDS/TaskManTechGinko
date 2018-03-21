8e3dc245e36b385f9ac1c71980ae1a5906b7ff44
premkmr <premkmr498@gmail.com>
2018-03-21 09:34:29 +0530

    Added new files for weekly site and zone

---
+++ Webadmin/src/main/java/com/ts/app/web/rest/dto/WeeklyZone.java
@@ -0,0 +1,22 @@
+package com.ts.app.web.rest.dto;
+
+public class WeeklyZone {
+	
+	private double rating;
+	private Long day;
+	
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public long getDay() {
+		return day;
+	}
+	public void setDay(Long day) {
+		this.day = day;
+	}
+	
+
+}
