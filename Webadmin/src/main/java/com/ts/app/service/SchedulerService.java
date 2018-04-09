a37d92371dbe0bee618d43b12773c49c8b232e3d
Gnanaprakash <gnanaprakash.r@uds.in>
2018-04-09 20:20:26 +0530

    changes to attendance report generation

--- Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
+++ Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
@@ -447,7 +447,7 @@ public class SchedulerService extends AbstractService {
 								Calendar currCal = Calendar.getInstance();
 								currCal.add(Calendar.HOUR_OF_DAY,  1);
 								long timeDiff = currCal.getTimeInMillis() - startCal.getTimeInMillis();
-								if(currCal.equals(startCal) || (timeDiff >= 0 && timeDiff <= 7200000)) { //within 2 hours of the shift start timing.
+								if(currCal.equals(startCal) || (timeDiff > 0 && timeDiff <= 7200000)) { //within 2 hours of the shift start timing.
 									long empCntInShift = employeeRepository.findEmployeeCountBySiteAndShift(site.getId(), shift.getStartTime(), shift.getEndTime());
 									if(empCntInShift == 0) {
 										empCntInShift = employeeRepository.findCountBySiteId(site.getId());
