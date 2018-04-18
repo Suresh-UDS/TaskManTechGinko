9719d51abb13896e66530b452b8c90c35498d0d7
Gnanaprakash <gnanaprakash.r@uds.in>
2018-04-18 11:34:56 +0530

    disabled caching for feedback questions

--- Webadmin/src/main/java/com/ts/app/domain/FeedbackQuestion.java
+++ Webadmin/src/main/java/com/ts/app/domain/FeedbackQuestion.java
@@ -17,8 +17,8 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
 @Table(name = "feedback_questions")
-@Cacheable(true)
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+//@Cacheable(true)
+//@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class FeedbackQuestion extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
