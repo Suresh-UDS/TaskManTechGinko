8d48ee58f5946531f64dbd6257695f8eb152c168
Muthukumarasami M <muthukumarasami23@gmail.com>
2018-05-14 15:33:40 +0530

    recheck

--- Webadmin/src/main/webapp/scripts/components/header/header.controller.js
+++ Webadmin/src/main/webapp/scripts/components/header/header.controller.js
@@ -3,7 +3,6 @@
 angular.module('timeSheetApp')
     .controller('HeaderController', function ($rootScope,$scope, $location, $state, 
         Auth, Principal, ENV, $timeout) {
-        $rootScope.accountName;
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
@@ -41,18 +40,21 @@ angular.module('timeSheetApp')
                  console.log('current user' +JSON.stringify(response.login));
                  if(response.firstName != null){
 
-                  $rootScope.accountName = response.firstName;
+                  $rootScope.accountNames = response.firstName;
                     if(response.lastName != null){
-                     $rootScope.accountName += " " + response.lastName;
+                     $rootScope.accountNames += " " + response.lastName;
                    }
-                 }else{
-                    $rootScope.accountName = response.login;
+                 }
+                 else{
+                    $rootScope.accountNames = response.login;
                  }
 
                  //alert($rootScope.accountName);
                  
              });
         };
+
+        $rootScope.inits();
              
              
 
