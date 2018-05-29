63f5daa4c2e42b48ffdeee3fc11ee219d9271c46
Praveen Sekar <praveens@techginko.com>
2018-05-29 14:17:44 +0530

    print function

--- Webadmin/src/main/webapp/scripts/app/admin/feedback/feedback.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/feedback/feedback.controller.js
@@ -360,6 +360,15 @@ angular.module('timeSheetApp')
             window.print();
         }
 
+
+        $scope.printToCart = function(printSectionId) {
+            var innerContents = document.getElementById(printSectionId).innerHTML;
+            var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+            popupWinindow.document.open();
+            popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="assets/css/material-dashboard.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+            popupWinindow.document.close();
+        }
+
         $scope.next = function() {
             console.log("Calling next")
 
