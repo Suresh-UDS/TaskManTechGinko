90ae0e6bd408ba82a67c5b10799ca9f875394c9a
Gnanaprakash <gnanaprakash.r@uds.in>
2018-05-29 13:49:01 +0530

    corrected table name spelling

--- Webadmin/src/main/java/com/ts/app/domain/ParameterUOM.java
+++ Webadmin/src/main/java/com/ts/app/domain/ParameterUOM.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 
 
 @Entity
-@Table(name = "paramter_uom")
+@Table(name = "parameter_uom")
 public class ParameterUOM extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
