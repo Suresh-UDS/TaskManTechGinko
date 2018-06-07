b9d8be7aceab0add8cf214b8bea00bc9a22084ea
Muthukumarasami M <muthukumarasami23@gmail.com>
2018-06-07 20:14:27 +0530

    asset gen

--- Webadmin/src/main/webapp/scripts/app/admin/asset/asset.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/asset/asset.controller.js
@@ -323,6 +323,8 @@ angular.module('timeSheetApp')
                         console.log('zones list',$scope.zones);
                    });
                 }
+
+                $scope.genQrCodes();
         		
         		/*if($scope.asset.assetType) {
         			$scope.assetConfig = {};
@@ -478,8 +480,9 @@ angular.module('timeSheetApp')
 
              
             if($stateParams.id){ 
+                alert($scope.assets.name);
                
-                $scope.assetConfig.assetTypeName = $scope.assetDetail.name;
+                $scope.assetConfig.assetTypeName = $scope.assets.name;
                 $scope.assetConfig.assetId = $stateParams.id;
             }
             else if($scope.assetGen.id){
@@ -539,7 +542,6 @@ angular.module('timeSheetApp')
                         $scope.assetGen.id=response.data.id;
                         $scope.success = 'OK';
                         $scope.showNotifications('top','center','success','Asset Added');
-                        $scope.selectedSite = null;
                         $scope.loadAssets();
 
                         //$location.path('/assets');
@@ -567,6 +569,13 @@ angular.module('timeSheetApp')
 
        $scope.createQrCode= function(){  
 
+        if(!$scope.assetGen.id){
+            
+          $scope.showNotifications('top','center','danger','Please create asset..');
+
+          return false;
+        }
+
         if($scope.assetGen.id){
             
             var qr = {id:$scope.assetGen.id,code:$scope.assetGen.assetcode}
@@ -584,9 +593,13 @@ angular.module('timeSheetApp')
 
        /* View QR code by asset id */
 
-       $scope.genQrCodes= function(){  
+       $scope.genQrCodes= function(){ 
 
-              var qr_id ={id:$scope.assetGen.id};
+              if($scope.asset.id){
+                var qr_id ={id:$scope.asset.id};
+              }else if($scope.assetGen.assetId){
+                var qr_id ={id:$scope.assetGen.id};
+              }
               $rootScope.loadingStart();
               $scope.qr_img = "";
 
@@ -607,12 +620,16 @@ angular.module('timeSheetApp')
         	    $scope.asset.siteId = $scope.asset.selectedSite.id;
         	    delete $scope.asset.selectedSite;
             }
-        	console.log('asset details ='+ JSON.stringify($scope.asset));
+        	console.log('Edit asset details ='+ JSON.stringify($scope.asset));
+
+            return false;
         	//$scope.asset.assetStatus = $scope.selectedStatus.name;
         	//var post = $scope.isEdit ? AssetComponent.update : AssetComponent.create
         	AssetComponent.update($scope.asset).then(function () {
 
                 $scope.success = 'OK';
+                 $scope.showNotifications('top','center','success','Asset Added');
+                 $scope.loadAssets();
 
             	$location.path('/assets');
 
@@ -846,7 +863,7 @@ angular.module('timeSheetApp')
 
             if($stateParams.id){
             	if($scope.asset.assetType){
-            	    $scope.parameterConfig.assetType = $scope.asset.assetType;
+            	    $scope.parameterConfig.assetType = $scope.asset.assetTypeName;
             	    $scope.parameterConfig.assetId = $stateParams.id;
             	}
             	if($scope.selectedParameter){
