2955def7ee896acf5633e54c506c4ce154bc799f
Gnanaprakash <gnanaprakash.r@uds.in>
2018-06-09 09:54:56 +0530

    fixed issue with attendance detailed report date

--- Webadmin/src/main/java/com/ts/app/web/rest/ReportResource.java
+++ Webadmin/src/main/java/com/ts/app/web/rest/ReportResource.java
@@ -59,7 +59,7 @@ public class ReportResource {
 	public ResponseEntity<?> sendDetailedAttendanceReport(@RequestParam(value = "date", required = false) Date attnDate) {
 		if(attnDate == null) {
 			Calendar currCal = Calendar.getInstance();
-			currCal.add(Calendar.DAY_OF_MONTH, currCal.get(Calendar.DAY_OF_MONTH) - 1);
+			currCal.add(Calendar.DAY_OF_YEAR, -1);
 			currCal.set(Calendar.HOUR_OF_DAY, 0);
 			currCal.set(Calendar.MINUTE,0);
 			attnDate = currCal.getTime();
