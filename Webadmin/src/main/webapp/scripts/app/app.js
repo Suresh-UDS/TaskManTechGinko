6bb0cfd185daf9c9f6ea597c29a863d664777eac
DESKTOP-IMRE38E\CLSS-LAP007 <praveens@techginko.com>
2018-06-25 11:45:58 +0530

    pageto top loading speed change

--- Webadmin/src/main/webapp/scripts/app/app.js
+++ Webadmin/src/main/webapp/scripts/app/app.js
@@ -119,7 +119,7 @@ angular.module('timeSheetApp', ['LocalStorageModule',
 
         $rootScope.loadPageTop = function(){
 
-            $("#loadPage").animate({scrollTop: 0}, 2000);
+            $("#loadPage").animate({scrollTop: 0}, 1500);
         }
 
 
