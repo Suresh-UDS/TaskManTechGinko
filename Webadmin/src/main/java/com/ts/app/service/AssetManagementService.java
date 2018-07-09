3ae42ff73d72992a7527bdda9fe924e11ea03165
yokeshwaran rajaraman <yokeshr@techginko.com>
2018-07-09 11:59:37 +0530

    changes for generate and get QR code to return map object.

--- Webadmin/src/main/java/com/ts/app/service/AssetManagementService.java
+++ Webadmin/src/main/java/com/ts/app/service/AssetManagementService.java
@@ -543,13 +543,14 @@ public class AssetManagementService extends AbstractService {
 		assetRepository.save(asset);
 	}
 	
-	public AssetDTO generateAssetQRCode(long assetId, String assetCode) {
+	public Map<String, Object> generateAssetQRCode(long assetId, String assetCode) {
 		Asset asset = assetRepository.findOne(assetId);
 		long siteId = asset.getSite().getId();
 		String code = String.valueOf(siteId)+"_"+assetCode;
 		asset.setCode(code);
 		assetRepository.save(asset);
 		byte[] qrCodeImage = null;
+		Map<String, Object> qrCodeObject = new HashMap<>();
 //		String qrCodeBase64 = null;
 		AssetDTO assetDTO = new AssetDTO();
 		if (asset != null) {
@@ -558,7 +559,9 @@ public class AssetManagementService extends AbstractService {
 			String qrCodePath = env.getProperty("AWS.s3-qrcode-path");
 			if (org.apache.commons.lang3.StringUtils.isNotEmpty(qrCodePath)) {
 				assetDTO = s3ServiceUtils.uploadQrCodeFile(code, qrCodeImage, assetDTO);
-				assetDTO.setCode(code);
+				qrCodeObject.put("code", code);
+				qrCodeObject.put("url", assetDTO.getUrl());
+				qrCodeObject.put("imageName", assetDTO.getQrCodeImage());
 				asset.setQrCodeImage(assetDTO.getQrCodeImage());
 				assetRepository.save(asset);
 			}
@@ -567,12 +570,13 @@ public class AssetManagementService extends AbstractService {
 //			}
 	}
 		log.debug("*****************"+ asset.getId());
-		return assetDTO;
+		return qrCodeObject;
 	}
 
-	public AssetDTO getQRCode(long assetId) {
+	public Map<String, Object> getQRCode(long assetId) {
 		log.debug(">>> get QR Code <<<");
-		AssetDTO assetDTO = new AssetDTO();
+//		AssetDTO assetDTO = new AssetDTO();
+		Map<String, Object> hm = new HashMap<>();		
 		Asset asset = assetRepository.findOne(assetId);
 //		String qrCodeBase64 = null;
 		String imageFileUrl = "" ;
@@ -580,12 +584,12 @@ public class AssetManagementService extends AbstractService {
 		if (asset != null) {
 			imageFileUrl = cloudFrontUrl + bucketEnv + qrcodePath + asset.getQrCodeImage();
 			if (org.apache.commons.lang3.StringUtils.isNotBlank(imageFileUrl)) {
-				assetDTO.setCode(assetcode);
-				assetDTO.setUrl(imageFileUrl);
+				hm.put("code", assetcode);
+				hm.put("url", imageFileUrl);
 			}
 		}
 //		qrCodeBase64 = qrCodeBase64 + "." + assetcode;
-		return assetDTO;
+		return hm;
 		}
 
 	public ExportResult generateReport(List<AssetDTO> transactions, SearchCriteria criteria) {
