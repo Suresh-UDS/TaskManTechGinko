acb2d0799ac2a2a4931c5c87bf205dee944bfecd
Gnanaprakash <gnanaprakash.r@uds.in>
2018-07-22 18:16:04 +0530

    fix for multi checkin issue

--- Webadmin/src/main/java/com/ts/app/service/AttendanceService.java
+++ Webadmin/src/main/java/com/ts/app/service/AttendanceService.java
@@ -249,6 +249,17 @@ public class AttendanceService extends AbstractService {
 		log.debug("seach criteria"+" - " +sc.getEmployeeEmpId()+" - " +sc.getSiteId()+" - " +sc.getCheckInDateTimeFrom()+" - " +sc.getCheckInDateTimeTo());
 		SearchResult<AttendanceDTO> result = findBySearchCrieria(sc);
 		//if(result == null || CollectionUtils.isEmpty(result.getTransactions())) {
+		boolean isCheckInAllowed = true;
+		if(result != null && !CollectionUtils.isEmpty(result.getTransactions())) {
+			List<AttendanceDTO> attns = result.getTransactions();
+			for(AttendanceDTO attnd : attns) {
+				if(attnd.getCheckOutTime() == null) {
+					isCheckInAllowed = false;
+					break;
+				}
+			}
+		}
+		if(isCheckInAllowed) {
 		    log.debug("no transactions" + attnDto.getEmployeeEmpId());
 			Employee emp = employeeRepository.findByEmpId(attnDto.getEmployeeEmpId());
 			Site site = siteRepository.findOne(attnDto.getSiteId());
@@ -318,6 +329,7 @@ public class AttendanceService extends AbstractService {
 
 		}
 		*/
+		}
 		return attnDto;
 	}
 
