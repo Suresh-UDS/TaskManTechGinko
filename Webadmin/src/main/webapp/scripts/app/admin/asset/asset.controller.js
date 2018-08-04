cd650697c30b0bd407044421f82c01a96d4f7806
muthukumarasami <37996770+muthukumarasami@users.noreply.github.com>
2018-08-04 17:51:59 +0530

    qr image print fixes

--- Webadmin/src/main/webapp/scripts/app/admin/asset/asset.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/asset/asset.controller.js
@@ -662,7 +662,7 @@ angular.module('timeSheetApp')
         $scope.editAsset = function(){
              //alert($stateParams.id);
              if(!$stateParams.id){
-                
+
                 $location.path('/assets');
              }
             console.log($stateParams.id);
