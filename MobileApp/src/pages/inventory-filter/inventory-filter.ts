44714d27b1265f4e44a0ae4b8a73c96cf5bd8baa
arthimala <arthi5196@gmail.com>
2018-08-06 12:45:44 +0530

    inventory-filter screen

---
+++ MobileApp/src/pages/inventory-filter/inventory-filter.ts
@@ -0,0 +1,132 @@
+import { Component } from '@angular/core';
+import {NavController, NavParams, ViewController} from "ionic-angular";
+import {SiteService} from "../service/siteService";
+import {AssetService} from "../service/assetService";
+import {componentService} from "../service/componentService";
+
+/**
+ * Generated class for the InventoryFilter page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+@Component({
+  selector: 'page-inventory-filter',
+  templateUrl: 'inventory-filter.html',
+})
+export class InventoryFilter {
+
+    clientList:any;
+    siteList:any;
+    assetGroup:any;
+    selectedProject:any;
+    selectedSite:any;
+    msg:any;
+    scrollSite:any;
+    activeSite:any;
+    selectedAssetGroup:any;
+    searchCriteria:any;
+    selectOptions:any;
+    page:1;
+    totalPages:0;
+    pageSort:15;
+    count=0;
+    assetType:any;
+    selectedAssetType:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public component:componentService,
+              public siteService:SiteService, public assetService:AssetService) {
+  }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad Inventory Filter');
+        this.component.showLoader('Getting Project');
+        this.selectOptions={
+            cssClass: 'selectbox-popover'
+        }
+        this.siteService.getAllProjects().subscribe(
+            response=>{
+                console.log("====project======");
+                console.log(response);
+                this.clientList=response;
+                this.selectedProject = this.clientList[0];
+                this.selectSite(this.selectedProject);
+                console.log('select default value:');
+                this.component.closeLoader();
+            },
+            error=>{
+                if(error.type==3)
+                {
+                    this.msg='Server Unreachable'
+                }
+                this.component.showToastMessage(this.msg,'bottom');
+                this.component.closeLoader();
+            }
+        )
+
+        this.assetService.getAssetType().subscribe(
+            response=>
+            {
+                console.log("Get Asset type Response");
+                console.log(response);
+                this.assetType = response;
+            },error=>
+            {
+                console.log("Get Asset Type  Reading");
+                console.log(error);
+            }
+        )
+        this.assetService.getAssetGroup().subscribe(
+            response=>
+            {
+                console.log("Get Asset Group Response");
+                console.log(response);
+                this.assetGroup = response;
+            },error=>
+            {
+                console.log("Get Asset Group Reading");
+                console.log(error);
+            }
+        )
+
+    }
+
+    selectSite(project)
+    {
+        this.selectedProject = project;
+        this.scrollSite = true;
+        this.siteService.findSitesByProject(project.id).subscribe(
+            response=>{
+                console.log("====Site By ProjectId======");
+                console.log(response);
+                this.siteList=response;
+                console.log(this.siteList);
+            },
+            error=>{
+                if(error.type==3)
+                {
+                    this.msg='Server Unreachable'
+                }
+                this.component.showToastMessage(this.msg,'bottom');
+            }
+        )
+    }
+
+    highLightSite(index,site){
+        console.log("Selected Site");
+        console.log(site);
+        this.activeSite= index;
+        this.selectedSite = site;
+    }
+
+    dismiss(){
+
+        this.viewCtrl.dismiss();
+    }
+
+
+
+
+
+
+}
--- MobileApp/src/pages/inventory-master/inventory-master.ts
+++ MobileApp/src/pages/inventory-master/inventory-master.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
-import {NavController, NavParams} from "ionic-angular";
+import {Component, Inject} from '@angular/core';
+import {AlertController, ModalController, NavController, NavParams} from "ionic-angular";
+import{InventoryFilter} from "../inventory-filter/inventory-filter";
+import {componentService} from "../service/componentService";
+import {ApplicationConfig, MY_CONFIG_TOKEN} from "../service/app-config";
+import {FileTransferObject, FileTransfer} from "@ionic-native/file-transfer";
+import {SQLite} from "@ionic-native/sqlite";
+import {QRScanner} from "@ionic-native/qr-scanner";
+import {AssetService} from "../service/assetService";
+import {Network} from "@ionic-native/network";
+import {DBService} from "../service/dbService";
+import {Diagnostic} from "@ionic-native/diagnostic";
 
 /**
  * Generated class for the InventoryMaster page.
@@ -13,11 +23,58 @@ import {NavController, NavParams} from "ionic-angular";
 })
 export class InventoryMaster {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    searchCriteria:any;
+    page:1;
+    totalPages:0;
+    open:any;
+    qr:any;
+
+
+
+    database:any;
+    db:any;
+    fileTransfer: FileTransferObject = this.transfer.create();
+
+  constructor(@Inject(MY_CONFIG_TOKEN) private config:ApplicationConfig,private transfer: FileTransfer,
+              public modalCtrl:ModalController,private diagnostic: Diagnostic,private sqlite: SQLite,
+              public componentService:componentService, public navCtrl: NavController, public navParams: NavParams,
+              public modalController:ModalController, public qrScanner:QRScanner, public assetService:AssetService,
+              public dbService:DBService,private network:Network,private alertCtrl:AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InventoryMaster');
   }
+
+    openFilter()
+    {
+        this.open = false;
+        console.log("Opening filter modal");
+        let modal = this.modalController.create(InventoryFilter,{},{cssClass : 'asset-filter',showBackdrop : true});
+        modal.onDidDismiss(data=>{
+            console.log("Modal dismissed");
+            this.open = true;
+            console.log(data);
+            var searchCriteria = {
+                siteId:data.siteId,
+                projectId:data.projectId,
+            };
+            this.assetService.searchAssets(searchCriteria).subscribe(
+                response=>{
+                    this.componentService.closeLoader()
+                    console.log("Asset search filters response");
+                    console.log(response)
+                },err=>{
+                    this.componentService.closeLoader();
+                    console.log("Error in filtering assets");
+                    console.log(err);
+                }
+            )
+            // this.getAsset(searchCriteria);
+
+        });
+        modal.present();
+
+    }
 
 }
