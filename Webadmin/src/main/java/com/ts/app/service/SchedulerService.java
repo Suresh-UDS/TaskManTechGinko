bb5e4ab8919b56560cacc3bfb3c15a42ccb2320b
Gnanaprakash <gnanaprakash.r@uds.in>
2018-08-11 23:47:13 +0530

    merged from Release-1.0

--- Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
+++ Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
@@ -363,21 +363,21 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-	//@Scheduled(cron = "0 */30 * * * ?")
+	@Scheduled(cron = "0 */30 * * * ?")
 	public void attendanceShiftReportSchedule() {
 		Calendar cal = Calendar.getInstance();
 		schedulerHelperService.generateDetailedAttendanceReport(cal.getTime(), true, false, false);
 	}
 
 
-	//@Scheduled(cron = "0 */30 * 1/1 * ?") // send detailed attendance report
+	@Scheduled(cron = "0 */30 * 1/1 * ?") // send detailed attendance report
 	public void attendanceDetailReportSchedule() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DAY_OF_YEAR, -1);
 		schedulerHelperService.generateDetailedAttendanceReport(cal.getTime(), false, true, false);
 	}
 
-	//@Scheduled(cron="0 */30 * * * ?") // runs every 30 mins
+	@Scheduled(cron="0 */30 * * * ?") // runs every 30 mins
 	public void attendanceCheckOutTask() {
 		schedulerHelperService.autoCheckOutAttendance();
 	}
