36fc986bd75b0b67a777ff06d88b65657f44ae0f
yokeshwaran rajaraman <yokeshr@techginko.com>
2018-08-13 12:28:19 +0530

    client side integration for inventory add and list.

--- Webadmin/src/main/webapp/scripts/app/admin/inventory/inventory.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/inventory/inventory.controller.js
@@ -1,10 +1,161 @@
 'use strict';
 
 angular.module('timeSheetApp')
-    .controller('InventoryController', function ($rootScope, $scope, $state, $timeout, ProjectComponent, SiteComponent,$http,$stateParams,$location) {
+    .controller('InventoryController', function ($rootScope, $scope, $state, $timeout, ProjectComponent, SiteComponent,$http,$stateParams,$location,
+    		ManufacturerComponent, InventoryComponent) {
 
           
     	$rootScope.loginView = false;
+    	$scope.inventory = {};
+    	$scope.client = {};
+    	$scope.projectSite = {};
+    	$scope.selectedManufacturer = {};
+    	$scope.selectedUOM;
+    	$scope.selectedItemGroup = {};
+    	$scope.materialItemGroup = {};
+    	$scope.pages = { currPage : 1};
+    	
+    	$scope.refreshPage = function() { 
+    		 $scope.pages = {
+    	                currPage: 1,
+    	                totalPages: 0
+    	            }
+    	}
+    	
+    	$scope.showNotifications= function(position,alignment,color,msg){
+
+            /*if(nottifShow == true){*/
+               $rootScope.overlayShow();
+               demo.showNotification(position,alignment,color,msg);
+
+            /*}else if(nottifShow == false){*/
+                $timeout(function() {
+                  $rootScope.overlayHide() ;
+                }, 5000);
+
+            /*}*/
+
+        }
+    	
+    	$scope.loadProjects = function () {
+            ProjectComponent.findAll().then(function (data) {
+                console.log("Loading all projects")
+                $scope.projects = data;
+            });
+        }
+    	
+    	$scope.loadAllSites = function () {
+    		if($scope.client) { 
+    			ProjectComponent.findSites($scope.client.id).then(function (data) {
+                    $scope.searchSite = null;
+                    $scope.sites = data;
+                });
+    		}
+        }
+    	
+    	$scope.loadManufacturer = function () {
+            ManufacturerComponent.findAll().then(function (data) {
+                //console.log("Loading all Manufacturer -- " , data);
+                $scope.manufacturers = data;
+                $scope.loadingStop();
+            });
+        }
+    	
+    	$scope.loadUOM = function() {
+    		InventoryComponent.getMaterialUOM().then(function(data){
+    			console.log(data);
+    			$scope.materialUOMs = data;
+    		});
+    	}
+    	
+    	$scope.cancelInventory = function(){
+            $location.path('/inventory-list');
+       }
+    	
+    	/* Add item group */
+
+        $scope.addMaterialItemGroup = function () {
+            console.log($scope.materialItemGroup);
+             $scope.loadingStart();
+            if($scope.materialItemGroup){
+                console.log("MaterialItmGroup Group entered");
+                InventoryComponent.createItemGroup($scope.materialItemGroup).then(function (response) {
+                    console.log(response);
+                    $scope.materialItemGroup = "";
+                    $scope.showNotifications('top','center','success','Item group has been added Successfully!!');
+                    $scope.loadMaterialItmGroup();
+
+                }).catch(function(){
+                $scope.loadingStop();
+                $scope.showNotifications('top','center','danger','Unable to add Item group. Please try again later..');
+                $scope.error = 'ERROR';
+            });
+            }else{
+                console.log("Item Group not entered");
+            }
+        }
+        
+        $scope.loadMaterialItmGroup = function () {
+        	InventoryComponent.loadItemGroup().then(function (data) {
+                $scope.materialItmGroups = data;
+                $scope.loadingStop();
+            });
+        }
+        
+        $scope.loadMaterials = function() { 
+        	$scope.search();
+        	$location.path('/inventory-list');
+        }
+    	
+    	$scope.saveInventory = function() {
+    		if($scope.client){
+    			$scope.inventory.projectId = $scope.client.id;
+    		}
+    		
+    		if($scope.projectSite){
+    			$scope.inventory.siteId = $scope.projectSite.id;
+    		}
+    		
+    		if($scope.selectedManufacturer) { 
+    			$scope.inventory.manufacturerId = $scope.selectedManufacturer.id;
+    		}
+    		
+    		if($scope.selectedItemGroup) {
+    			$scope.inventory.itemGroup = $scope.selectedItemGroup.itemGroup;
+    		}
+    		
+    		if($scope.selectedUOM){
+    			$scope.inventory.uom = $scope.selectedUOM;
+    		}
+    		
+    		console.log(JSON.stringify($scope.inventory));
+    		
+    		InventoryComponent.create($scope.inventory).then(function(data) { 
+    			console.log(data);
+                $scope.loadingStop();
+                $scope.inventory = "";
+                $scope.showNotifications('top','center','success','Material has been added Successfully!!');
+                $scope.loadMaterials();
+    		}).catch(function (response) {
+                $scope.loadingStop();
+                $scope.btnDisabled= false;
+                $scope.success = null;
+                console.log('Error - '+ response.data);
+                console.log('status - '+ response.status + ' , message - ' + response.data.message);
+                if (response.status === 400 && response.data.message === 'error.duplicateRecordError') {
+                        $scope.errorAssetsExists = 'ERROR';
+                    $scope.showNotifications('top','center','danger','Material Already Exists');
+                } else {
+                    $scope.showNotifications('top','center','danger','Unable to creating Inventory. Please try again later..');
+                    $scope.error = 'ERROR';
+                }
+            });
+    		
+    		
+    	}
+    	
+    	
+    	
     	$scope.search = function () {
         	var currPageVal = ($scope.pages ? $scope.pages.currPage : 1);
         	if(!$scope.searchCriteria) {
@@ -36,11 +187,11 @@ angular.module('timeSheetApp')
 	        	}
         	}
         	console.log($scope.searchCriteria);
-        	InventorylistComponent.search($scope.searchCriteria).then(function (data) {
-                $scope.inventorylist = data.transactions;
+        	InventoryComponent.search($scope.searchCriteria).then(function (data) {
+        		console.log(data);
+                $scope.inventorylists = data.transactions;
                 $scope.loadingStop();
                 $scope.inventorylistLoader = true;
-                console.log($scope.checklists);
                 $scope.pages.currPage = data.currPage;
                 $scope.pages.totalPages = data.totalPages;
                 if($scope.checklists == null){
@@ -63,10 +214,15 @@ angular.module('timeSheetApp')
 			//init load
 			$scope.initLoad = function(){ 
 			     $scope.loadPageTop(); 
-			     $scope.loadInventory(); 
-			     $scope.init(); 
+			     $scope.loadProjects();
+			     $scope.loadManufacturer();
+			     $scope.loadUOM();
 			  
 			 }
+			
+			$scope.initList = function() { 
+				$scope.loadMaterials();
+			}
 
 			//Loading Page go to top position
 			$scope.loadPageTop = function(){
---
+++ Webadmin/src/main/webapp/scripts/components/admin/inventory.service.js
@@ -0,0 +1,83 @@
+'use strict';
+
+angular.module('timeSheetApp')
+    .factory('InventoryComponent', function InventoryComponent(Inventory,$http) {
+        return {
+
+            create : function(inventory,callback){
+                var cb = callback || angular.noop;
+                return $http.post('api/save/inventory', inventory).then(
+                    function (response) {
+                        return cb(response);
+                    }).catch(
+                    function (err) {
+                        console.log(JSON.stringify(err));
+                        return cb(err);
+                    })
+
+            },
+            update : function(inventory,callback){
+                var cb = callback || angular.noop;
+                return $http.put('api//update/inventory',inventory).then(
+                    function (response) {
+                        return cb(response);
+                    }).catch(
+                    function (err) {
+                        console.log(JSON.stringify(err));
+                        return cb(err);
+                    })
+
+            },
+
+            remove: function (id, callback) {
+
+                var cb = callback || angular.noop;
+
+                return  $http.delete('api//delete/inventory/'+id).then(
+                    function (response) {
+                        return cb(response);
+                    }).catch(
+                    function (err) {
+                        console.log(JSON.stringify(err));
+                        return cb(err);
+                    })
+            },
+            findAll: function () {
+				return $http.get('api/findAll').then(function (response) {
+					return response.data;
+				});
+			},            
+            findById : function(id){
+                return $http.get('api/inventory/'+id).then(function (response) {
+                    return response.data;
+                });
+            },
+            search: function(searchCriteria) {
+                return $http.post('api/inventory/search',searchCriteria).then(function (response) {
+                    return response.data;
+                });
+            },
+            getMaterialUOM : function() { 
+            	return $http.get('api/material/uom').then(function(response) {
+            		return response.data;
+            	});
+            },
+            createItemGroup : function(itemGroup, callback) {
+            	var cb = callback || angular.noop;
+                return $http.post('api/materialItemgroup', itemGroup).then(
+                    function (response) {
+                        return cb(response);
+                    }).catch(
+                    function (err) {
+                        console.log(JSON.stringify(err));
+                        return cb(err);
+                    })
+
+            },
+            loadItemGroup : function() { 
+            	return $http.get('api/materialItemgroup').then(function(response) { 
+            		return response.data;
+            	});
+            }
+        };
+    });
