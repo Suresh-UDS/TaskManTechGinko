d6dc156c800bc15463a11aa55e9cc1d1a3c6b62a
Gnanaprakash <gnanaprakash.r@uds.in>
2018-08-21 17:15:38 +0530

    changes for ios app build

--- MobileApp/src/pages/service/app-config.ts
+++ MobileApp/src/pages/service/app-config.ts
@@ -12,8 +12,8 @@ export interface ApplicationConfig
 
 export const AppConfig: ApplicationConfig={
     // Url: "http://192.168.1.4:8088/",
-    Url: "http://ec2-54-169-225-123.ap-southeast-1.compute.amazonaws.com:8088/",
-    //Url: "https://taskmanapi.uds.in/",
+    //Url: "http://ec2-54-169-225-123.ap-southeast-1.compute.amazonaws.com:8088/",
+    Url: "https://taskmanapi.uds.in/",
 };
 
 export const MY_CONFIG_TOKEN = new InjectionToken<ApplicationConfig>('config');
