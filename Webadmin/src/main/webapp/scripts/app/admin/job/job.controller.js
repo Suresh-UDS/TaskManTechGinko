faa0a8143b13589e1c766e4b237374b9442e84c2
muthukumarasami <muthukumarasmi23@gmail.com>
2018-09-04 17:38:36 +0530

    emp list

--- Webadmin/src/main/webapp/scripts/app/admin/job/job.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/job/job.controller.js
@@ -370,7 +370,7 @@ angular.module('timeSheetApp')
             }else{
                 $scope.searchCriteria.siteId = null;
             }
-        		//$scope.searchCriteria.list = true;
+        		$scope.searchCriteria.list = true;
                 $scope.searchCriteria.isReport = true;
 
                 $scope.employees = "";
