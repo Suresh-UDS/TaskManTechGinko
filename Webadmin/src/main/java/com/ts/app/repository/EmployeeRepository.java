d0f53746570850ef88f5c1eee5f4dcaa40f55896
Gnanaprakash <gnanaprakash.r@uds.in>
2018-09-06 16:45:46 +0530

    changes for employee filter issue

--- Webadmin/src/main/java/com/ts/app/repository/EmployeeRepository.java
+++ Webadmin/src/main/java/com/ts/app/repository/EmployeeRepository.java
@@ -48,6 +48,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	@Query("SELECT distinct e FROM Employee e WHERE e.empId IN :empIds and e.active='Y' and e.isLeft = FALSE and (client = :isClient or client = FALSE) order by e.empId ")
 	Page<Employee> findAllByEmpCodes(@Param("empIds") List<String> empIds, @Param("isClient") boolean isClient, Pageable pageRequest);
 
+	@Query("SELECT distinct e FROM Employee e join e.projectSites p WHERE p.site.id IN (:siteIds) and e.empId IN :empIds and e.active='Y' and e.isLeft = FALSE and (client = :isClient or client = FALSE) order by e.empId ")
+	Page<Employee> findAllBySiteIdsAndEmpCodes(@Param("siteIds") List<Long> siteIds,@Param("empIds") List<String> empIds, @Param("isClient") boolean isClient, Pageable pageRequest);
+
 	@Query("SELECT distinct e FROM Employee e WHERE e.id IN :empIds and e.active='Y' and e.isLeft = FALSE and (client = :isClient or client = FALSE) order by e.empId ")
 	Page<Employee> findAllByEmpIds(@Param("empIds") List<Long> empIds, @Param("isClient") boolean isClient, Pageable PageRequest);
 
@@ -120,8 +123,11 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query("SELECT distinct e FROM Employee e join e.projectSites ps WHERE ps.project.id = :projectId and e.name like '%' || :empName || '%' and e.active='Y' and e.isLeft = FALSE and (client = :isClient or client = FALSE) order by e.empId")
 	Page<Employee> findByProjectAndEmployeeName(@Param("projectId") long projectId, @Param("empName") String empName, @Param("isClient") boolean isClient, Pageable pageRequest);
 
-    @Query("SELECT e FROM Employee e WHERE e.name like '%' || :empName || '%' and e.active='Y' and (client = :isClient or client = FALSE) order by e.empId")
-	Page<Employee> findByEmployeeName(@Param("empName") String empName, @Param("isClient") boolean isClient, Pageable pageRequest);
+    @Query("SELECT distinct e FROM Employee e join e.projectSites ps WHERE  e.name like '%' || :empName || '%' and ps.site.id in (:siteIds) and e.active='Y' and (client = :isClient or client = FALSE) order by e.empId")
+	Page<Employee> findByEmployeeName(@Param("siteIds") List<Long> siteIds, @Param("empName") String empName, @Param("isClient") boolean isClient, Pageable pageRequest);
+
+    @Query("SELECT distinct e FROM Employee e  WHERE  e.name like '%' || :empName || '%' and e.active='Y' and (client = :isClient or client = FALSE) order by e.empId")
+	Page<Employee> findByEmployeeName( @Param("empName") String empName, @Param("isClient") boolean isClient, Pageable pageRequest);
 
     /*
     @Query("SELECT e FROM Employee e , User u WHERE ((e.id = :employeeId and e.project.id = :projectId) or e.site.id = :siteId) and e.user.id = u.id and u.userGroup.id = :userGroupId and e.active='Y' order by e.empId")
