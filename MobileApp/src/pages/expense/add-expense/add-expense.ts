715d2847b4a1d2bcbe5e003c7a204b66559a26e1
prasanna <prasanna.r@clsslabs.com>
2018-09-10 16:52:48 +0530

    add expense changes

--- MobileApp/src/pages/expense/add-expense/add-expense.ts
+++ MobileApp/src/pages/expense/add-expense/add-expense.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import {NavController, NavParams,ViewController} from "ionic-angular";
+import {ModalController, NavController, NavParams, ViewController} from "ionic-angular";
+import {DatePickerProvider} from "ionic2-date-picker";
+import {DatePicker} from "@ionic-native/date-picker";
+import {SiteService} from "../../service/siteService";
+import {componentService} from "../../service/componentService";
 
 /**
  * Generated class for the AddExpense page.
@@ -13,17 +17,122 @@ import {NavController, NavParams,ViewController} from "ionic-angular";
   templateUrl: 'add-expense.html',
 })
 export class AddExpense {
+  selectOptions: { cssClass: string; };
+  siteList: any;
+  scrollSite: boolean;
+  msg: string;
+  selectedProject: any;
+  clientList: any;
+  searchCriteria: any;
+  selectDate: Date;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public viewCtrl:ViewController) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams,public viewCtrl:ViewController,
+              private datePicker: DatePicker, private modalCtrl: ModalController,private siteService:SiteService,
+              private component: componentService)
+  {  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddExpense');
+    this.component.showLoader('Getting Project');
+    this.selectOptions = {
+      cssClass: 'selectbox-popover'
+    }
+    this.siteService.getAllProjects().subscribe(
+      response => {
+        this.component.closeLoader();
+        console.log("project");
+        console.log(response);
+        this.clientList = response;
+        this.selectedProject = this.clientList[0];
+        this.selectSite(this.selectedProject);
+        console.log('select default value:');
+      },
+      error => {
+        this.component.closeLoader();
+        if (error.type == 3) {
+          this.msg = 'Server Unreachable'
+        }
+        this.component.showToastMessage(this.msg, 'bottom');
+      }
+    )
   }
 
-    dismiss(){
+  selectSite(project) {
+    this.selectedProject = project;
+    this.scrollSite = true;
+    this.siteService.findSitesByProject(project.id).subscribe(
+      response => {
+        console.log("Site By ProjectId");
+        console.log(response);
+        this.siteList = response;
+        console.log(this.siteList);
+      },
+      error => {
+        if (error.type == 3) {
+          this.msg = 'Server Unreachable';
+        }
+        this.component.showToastMessage(this.msg, 'bottom');
+      }
+    )
+  }
+
+  dismiss(){
         let data={'foo':'bar'};
         this.viewCtrl.dismiss(data);
     }
+  /*saveExpense() {
+    var expenseDetails = {
+      projectId: this.selectedProject.id,
+      siteId: this.selectedSite.id,
+      paymentType: this.selectedPaymentType,
+      currency: this.selectedCurrency.code,
+      description: this.description,
 
+
+      if (this.transactionMode = 'debit') {
+      expenseDate : new Date(this.selectedDate)
+    } else {
+      creditedDate : new Date(this.selectedDate)
+    }
+    if (this.selectedCategory && this.transactionMode = 'debit') {
+      expenseCategory: this.selectedCategory,
+    }
+
+    if (this.transactionMode = 'debit') {
+      debitAmount: this.selectedAmount
+    } else {
+      creditAmount: this.selectedAmount
+    }
+
+    if (this.billable) {
+      billable: this.billable
+    } else {
+      billable: false
+    }
+
+    if (this.reimbursable) {
+      reimbursable: this.reimbursable
+    } else {
+      reimbursable: false
+    }
+  }
+  }*/
+
+  showCalendar() {
+      this.datePicker.show({
+        date: new Date(),
+        mode: 'date',
+        androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK,
+        allowFutureDates:false
+      }).then(
+        date=>{
+          this.selectDate = date;
+          console.log("date:",date);
+        },
+          err=>console.log("Error occured while getting date:"+err)
+      );
+
+
+
+  }
 }
