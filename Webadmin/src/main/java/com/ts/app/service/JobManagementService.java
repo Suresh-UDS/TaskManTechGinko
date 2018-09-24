bcff74e7c02983bc8dc133dba64cea54b8a790a0
Karthick Kumararaja <karthickk@techginko.com>
2018-09-24 18:46:52 +0530

    location detail in job reports

--- Webadmin/src/main/java/com/ts/app/service/JobManagementService.java
+++ Webadmin/src/main/java/com/ts/app/service/JobManagementService.java
@@ -1077,8 +1077,11 @@ public class JobManagementService extends AbstractService {
 		if(job.getAsset() != null) {
 			dto.setAssetId(job.getAsset().getId());
 		}
+		dto.setBlock(job.getBlock());
+		dto.setFloor(job.getFloor());
+		dto.setZone(job.getZone());
 		dto.setSiteId(job.getSite().getId());
-		dto.setSiteName(job.getSite().getName()); 
+		dto.setSiteName(job.getSite().getName());
 		dto.setSiteProjectId(String.valueOf(job.getSite().getProject().getId()));
 		dto.setSiteProjectName(job.getSite().getProject().getName());
 		dto.setDescription(job.getDescription());
