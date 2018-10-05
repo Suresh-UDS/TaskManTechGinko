e344abbec3de1a10a7627a0991cabc5f48582a89
Gnanaprakash <gnanaprakash.r@uds.in>
2018-10-05 23:49:50 +0530

    updated schedulers

--- Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
+++ Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
@@ -200,7 +200,7 @@ public class SchedulerService extends AbstractService {
 
 //	@Scheduled(initialDelay = 60000, fixedRate = 1800000) // Runs every 30 mins
 	// @Scheduled(cron="30 * * * * ?") //Test to run every 30 seconds
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createDailyTask() {
 		if (env.getProperty("scheduler.dailyJob.enabled").equalsIgnoreCase("true")) {
             log.debug("Daily jobs enabled");
@@ -299,7 +299,7 @@ public class SchedulerService extends AbstractService {
 
 //	@Scheduled(initialDelay = 60000, fixedRate = 1800000) // Runs every 30 mins
 	// @Scheduled(cron="30 * * * * ?") //Test to run every 30 seconds
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createWeeklyTask() {
 		if (env.getProperty("scheduler.weeklyJob.enabled").equalsIgnoreCase("true")) {
 			Calendar cal = Calendar.getInstance();
@@ -396,7 +396,7 @@ public class SchedulerService extends AbstractService {
 
 //	@Scheduled(initialDelay = 60000, fixedRate = 1800000) // Runs every 30 mins
 	// @Scheduled(cron="30 * * * * ?") //Test to run every 30 seconds
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createMonthlyTask() {
 		if (env.getProperty("scheduler.monthlyJob.enabled").equalsIgnoreCase("true")) {
 			Calendar cal = Calendar.getInstance();
@@ -438,7 +438,7 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createFortnightlyTask() {
 		if (env.getProperty("scheduler.fortnightlyJob.enabled").equalsIgnoreCase("true")) {
 			Calendar cal = Calendar.getInstance();
@@ -471,7 +471,7 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createQuarterlyTask() {
 		if (env.getProperty("scheduler.quarterlyJob.enabled").equalsIgnoreCase("true")) {
 			Calendar cal = Calendar.getInstance();
@@ -503,7 +503,7 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createHalfYearlyTask() {
 		if (env.getProperty("scheduler.halfyearlyJob.enabled").equalsIgnoreCase("true")) {
 			Calendar cal = Calendar.getInstance();
@@ -535,7 +535,7 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-//	@Scheduled(cron = "0 0 23 1/1 * ?")
+	@Scheduled(cron = "0 0 23 1/1 * ?")
 	public void createYearlyTask() {
 		if (env.getProperty("scheduler.yearlyJob.enabled").equalsIgnoreCase("true")) {
 			Calendar cal = Calendar.getInstance();
@@ -567,7 +567,7 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-//	@Scheduled(initialDelay = 60000, fixedRate = 900000) // Runs every 15 mins
+	//@Scheduled(initialDelay = 60000, fixedRate = 900000) // Runs every 15 mins
 	public void overDueTaskCheck() {
 		schedulerHelperService.overdueJobReport();
 	}
@@ -694,14 +694,14 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-	@Scheduled(cron = "0 */30 * * * ?")
+	//@Scheduled(cron = "0 */30 * * * ?")
 	public void attendanceShiftReportSchedule() {
 		Calendar cal = Calendar.getInstance();
 		schedulerHelperService.generateDetailedAttendanceReport(cal.getTime(), true, false, false);
 	}
 
 
-	@Scheduled(cron = "0 */30 * 1/1 * ?") // send detailed attendance report
+	//@Scheduled(cron = "0 */30 * 1/1 * ?") // send detailed attendance report
 	public void attendanceDetailReportSchedule() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DAY_OF_YEAR, -1);
@@ -722,19 +722,19 @@ public class SchedulerService extends AbstractService {
 		schedulerHelperService.generateMusterRollAttendanceReport(siteId, startCal.getTime(), endCal.getTime(), true, false);
 	}
 
-	@Scheduled(cron="0 */30 * * * ?") // runs every 30 mins
+	//@Scheduled(cron="0 */30 * * * ?") // runs every 30 mins
 	public void attendanceCheckOutTask() {
 		schedulerHelperService.autoCheckOutAttendance();
 	}
 
-	@Scheduled(cron="0 0 9 * * ?")
+	//@Scheduled(cron="0 0 9 * * ?")
 	public void warrantyExpireAlert() {
 		schedulerHelperService.sendWarrantyExpireAlert();
 		schedulerHelperService.sendSchedulePPMJobsAlert();
 		schedulerHelperService.sendScheduleAMCJobsAlert();
 	}
 
-	@Scheduled(cron="0 */5 * * * ?") // runs every 30 mins
+	//@Scheduled(cron="0 */5 * * * ?") // runs every 30 mins
 	public void feedbackDetailReportSchedule() {
 		Calendar cal = Calendar.getInstance();
 		//cal.add(Calendar.DAY_OF_YEAR, -1);
@@ -1371,7 +1371,7 @@ public class SchedulerService extends AbstractService {
 			}
 		}
 	
-	@Scheduled(cron="0 */30 * * * ?")
+	//@Scheduled(cron="0 */30 * * * ?")
 	public void sendDaywiseReport() {
 		Calendar cal = Calendar.getInstance();
 		boolean isOnDemand = false;
