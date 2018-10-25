3f322ba7f8918f16afacbb59e416ad9fc854323c
Karthick Kumararaja <karthickk@techginko.com>
2018-10-25 11:19:11 +0530

    no message

--- Webadmin/src/main/java/com/ts/app/service/util/ImportUtil.java
+++ Webadmin/src/main/java/com/ts/app/service/util/ImportUtil.java
@@ -973,10 +973,12 @@ public class ImportUtil {
                 SiteDTO siteDTO = new SiteDTO();
                 EmployeeDTO employeeDTO = new EmployeeDTO();
                 EmployeeProjectSiteDTO employeeProjectSiteDTO = new EmployeeProjectSiteDTO();
-                String empId  = getCellValue(currentRow.getCell(1));
                 Long siteId = Long.valueOf(getCellValue(currentRow.getCell(0)));
+                String empId  = getCellValue(currentRow.getCell(1));
+                Long projectId= Long.valueOf(getCellValue(currentRow.getCell(2)));
 
                 Employee employee = employeeRepo.findByEmpId(empId);
+
                 log.debug("Employee found - "+employee.getName());
 
             }
