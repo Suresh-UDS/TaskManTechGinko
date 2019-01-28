a6f0377af7c56764cc3087c777d4e97b1b791f3a
yokeshwaran rajaraman <yokeshr@techginko.com>
2019-01-28 20:11:03 +0530

    Influx services are commenting all configs and files.

--- Webadmin/src/main/webapp/scripts/app/admin/dashboard/dashboard.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/dashboard/dashboard.controller.js
@@ -67,13 +67,13 @@ angular.module('timeSheetApp')
             $scope.loadAllProjects();
             $scope.loadAllSites();
             $scope.loadAllEmployee();
-            $scope.loadAllAttendanceCounts();
-            $scope.loadQuotationReportCounts();
-            $scope.loadJobReport();
-            $scope.loadingStart();
-            $scope.loadChartData();
-            $scope.loadTicketStatusFromInflux();
-            $scope.loadCharts();
+            // $scope.loadAllAttendanceCounts();
+            // $scope.loadQuotationReportCounts();
+            // $scope.loadJobReport();
+            // $scope.loadingStart();
+            // $scope.loadChartData();
+            // $scope.loadTicketStatusFromInflux();
+            // $scope.loadCharts();
         };
 
         // Load Charts function
@@ -1508,83 +1508,83 @@ angular.module('timeSheetApp')
 
 
 
-        $scope.ticketsAgeChart = Highcharts.chart('catgAgeTicketsCharts', {
-            chart: {
-                type: 'spline'
-            },
-            title: {
-                text: 'Monthly Tickets'
-            },
-            subtitle: {
-                text: ''
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
-                title: {
-                    text: 'Average Ticket Age'
-                },
-                labels: {
-                    formatter: function () {
-                        return this.value;
-                    }
-                }
-            },
-            tooltip: {
-                crosshairs: true,
-                shared: true
-            },
-            plotOptions: {
-                spline: {
-                    marker: {
-                        radius: 4,
-                        lineColor: '#666666',
-                        lineWidth: 1
-                    }
-                }
-            },
-            series: [{
-                name: 'Ac',
-                marker: {
-                    symbol: 'square'
-                },
-                data: [7.5, 7.9, 5.5, 17.5, 20.2, 24.5, 29.2, {
-                    y: 26.5
-                }, 32.3, 33.3, 38.9, 44.6]
-
-            },{
-                name: 'CLEANING',
-                marker: {
-                    symbol: 'circle'
-                },
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-                    y: 26.5
-                }, 23.3, 18.3, 13.9, 9.6]
-
-            },{
-                name: 'PLUMBING',
-                marker: {
-                    symbol: 'square'
-                },
-                data: [2.0, 9.9, 11.5, 17.5, 23.2, 27.5, 29.2, {
-                    y: 26.5
-                    // marker: {
-                    //     symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
-                    // }
-                }, 33.3, 40.3, 43.9, 12.6]
-
-            }, {
-                name: 'ELECTRICAL',
-                marker: {
-                    symbol: 'diamond'
-                },
-                data: [{
-                    y: 3.9
-                }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-            }]
-        });
+        // $scope.ticketsAgeChart = Highcharts.chart('catgAgeTicketsCharts', {
+        //     chart: {
+        //         type: 'spline'
+        //     },
+        //     title: {
+        //         text: 'Monthly Tickets'
+        //     },
+        //     subtitle: {
+        //         text: ''
+        //     },
+        //     xAxis: {
+        //         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        //             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        //     },
+        //     yAxis: {
+        //         title: {
+        //             text: 'Average Ticket Age'
+        //         },
+        //         labels: {
+        //             formatter: function () {
+        //                 return this.value;
+        //             }
+        //         }
+        //     },
+        //     tooltip: {
+        //         crosshairs: true,
+        //         shared: true
+        //     },
+        //     plotOptions: {
+        //         spline: {
+        //             marker: {
+        //                 radius: 4,
+        //                 lineColor: '#666666',
+        //                 lineWidth: 1
+        //             }
+        //         }
+        //     },
+        //     series: [{
+        //         name: 'Ac',
+        //         marker: {
+        //             symbol: 'square'
+        //         },
+        //         data: [7.5, 7.9, 5.5, 17.5, 20.2, 24.5, 29.2, {
+        //             y: 26.5
+        //         }, 32.3, 33.3, 38.9, 44.6]
+        //
+        //     },{
+        //         name: 'CLEANING',
+        //         marker: {
+        //             symbol: 'circle'
+        //         },
+        //         data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+        //             y: 26.5
+        //         }, 23.3, 18.3, 13.9, 9.6]
+        //
+        //     },{
+        //         name: 'PLUMBING',
+        //         marker: {
+        //             symbol: 'square'
+        //         },
+        //         data: [2.0, 9.9, 11.5, 17.5, 23.2, 27.5, 29.2, {
+        //             y: 26.5
+        //             // marker: {
+        //             //     symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+        //             // }
+        //         }, 33.3, 40.3, 43.9, 12.6]
+        //
+        //     }, {
+        //         name: 'ELECTRICAL',
+        //         marker: {
+        //             symbol: 'diamond'
+        //         },
+        //         data: [{
+        //             y: 3.9
+        //         }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        //     }]
+        // });
 
         $rootScope.ticketSingleGraph = function(){
            $scope.ticketSingleStackChart = Highcharts.chart('ticketSingleStackedCharts', {
