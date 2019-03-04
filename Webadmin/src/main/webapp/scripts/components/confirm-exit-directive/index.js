1ad385e2fdd258d11149a6124bbe5270824840d9
muthukumarasami <muthukumarasmi23@gmail.com>
2019-03-04 15:53:36 +0530

    without form submiting confirm msg throw while state change .

---
+++ Webadmin/src/main/webapp/scripts/components/confirm-exit-directive/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('confirmMsg',[])
+.directive('confirmOnExit', function() {
+    return {
+        link: function($scope, elem, attrs) {
+            /*window.onbeforeunload = function(){
+                if ($scope.form.$dirty) {
+                    return "The form is dirty, do you want to stay on the page?";
+                }
+            }*/
+            var form = $scope[attrs.name];
+            $scope.$on('$stateChangeStart', function(event, next, current) {
+                if (form.$dirty) {
+                    if(confirm("are you sure want to leave page without saving?")) {
+                        return true;
+                    }else{
+                        event.preventDefault();
+                    }
+                }
+            });
+        }
+    };
+})
+.directive('confirmOnExitTouch', function() {
+    return {
+        link: function($scope, elem, attrs) {
+            /*window.onbeforeunload = function(){
+                if ($scope.form.$dirty) {
+                    return "The form is dirty, do you want to stay on the page?";
+                }
+            }*/
+            var form = $scope[attrs.name];
+            $scope.$on('$stateChangeStart', function(event, next, current) {
+                if (form.$dirty || form.$touched) {
+                    if(confirm("are you sure want to leave page without saving?")) {
+                        return true;
+                    }else{
+                        event.preventDefault();
+                    }
+                }
+            });
+        }
+    };
+});
