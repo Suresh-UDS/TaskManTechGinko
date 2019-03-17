201ad6992e3ca31d5a004c2cc68fe61301ef6827
techginko <gnanaprakash@techginko.com>
2019-03-17 14:05:31 +0530

    Enabled sla and daywise report schedulers

--- Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
+++ Webadmin/src/main/java/com/ts/app/service/SchedulerService.java
@@ -807,7 +807,7 @@ public class SchedulerService extends AbstractService {
 		}
 	}
 
-	@Scheduled(cron = "0 */5 * * * ?")
+	@Scheduled(cron = "0 */30 * * * ?")
 	public void slaTicketEscalationNotification()
 	{
 		String mailStatus = "";
@@ -994,7 +994,7 @@ public class SchedulerService extends AbstractService {
 			}
 		}
 
-//	@Scheduled(cron="0 */30 * * * ?")
+    @Scheduled(cron="0 */30 * * * ?")
 	public void sendDaywiseReport() {
 		log.info("Daywise report scheduler invoked");
 		Calendar cal = Calendar.getInstance();
