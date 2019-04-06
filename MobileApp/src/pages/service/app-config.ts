3043c60f2be2e9ad7c2e14a527f9cc8fe6ecbbb8
Karthick Kumararaja <karthickk@techginko.com>
2019-04-06 16:04:37 +0530

    cloudfront url changed

--- MobileApp/src/pages/service/app-config.ts
+++ MobileApp/src/pages/service/app-config.ts
@@ -11,9 +11,9 @@ export interface ApplicationConfig
 export const AppConfig: ApplicationConfig={
     //Url: "http://52.221.57.9:8088/",
     //UAT Url: "http://13.127.251.152:8088/",
-      // Url: "http://13.234.119.73:8088/",
+    // Url: "http://13.234.119.73:8088/",
     Url: "https://taskmanadmin.uds.in/",
-      s3Bucket : "https://s3zappyweb.s3.ap-south-1.amazonaws.com/uat/expenseDocuments/"
+    s3Bucket : "https://s3.ap-south-1.amazonaws.com/prod/expenseDocuments/"
     // Url: "http://10.0.0.7:8088/",
     // Url: "https://taskmanadmin.uds.in/",
     // Url: "http://ec2-54-169-225-123.ap-southeast-1.compute.amazonaws.com:8088/", // - Dev Server
