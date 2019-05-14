b441e520ce620c78c69a26fd21fc156786ce62a1
Karthick Kumararaja <karthickk@techginko.com>
2019-05-14 17:25:38 +0530

    job save changes

--- Webadmin/src/main/java/com/ts/app/service/JobManagementService.java
+++ Webadmin/src/main/java/com/ts/app/service/JobManagementService.java
@@ -1222,28 +1222,14 @@ public class JobManagementService extends AbstractService {
 		Location location = null;
 		if(jobDTO.getLocationId() > 0) {
 			location = getLocation(jobDTO.getLocationId());
-		}else if(job !=null && job.getLocation()!=null && job.getLocation().getId() !=null && job.getLocation().getId()>0){
-		    location = getLocation(job.getLocation().getId());
         }
 		Asset asset = null;
 		if(jobDTO.getAssetId() > 0) {
 			asset = assetRepository.findOne(jobDTO.getAssetId());
-		}else if(job!=null && job.getAsset().getId()>0){
-		    asset = assetRepository.findOne(job.getAsset().getId());
         }
 		Ticket ticket = null;
 		if(jobDTO.getTicketId() > 0) {
 			ticket = getTicket(jobDTO.getTicketId());
-		}else if(job !=null && job.getTicket().getId()>0){
-		    ticket = getTicket(job.getTicket().getId());
-        }
-
-        if(jobDTO.getActive() !=null){
-		    job.setActive(jobDTO.getActive());
-        }else if(job!=null && job.getActive() !=null){
-            job.setActive(job.getActive());
-        }else{
-            job.setActive("Y");
         }
 		//update ticket status
 		if(ticket != null) {
