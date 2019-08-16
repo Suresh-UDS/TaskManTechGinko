398cdaa374b868bf1276756d365a33ef1fb8a317
karthickcdm <karthickkumararajak@gmail.com>
2019-08-16 16:06:07 +0530

    on boarding repository

--- Webadmin/src/main/java/com/ts/app/repository/OnboardingUserConfigRepository.java
+++ Webadmin/src/main/java/com/ts/app/repository/OnboardingUserConfigRepository.java
@@ -20,19 +20,20 @@ public interface OnboardingUserConfigRepository extends JpaRepository<Onboarding
     @Transactional
     void deleteByUserId(@Param("userId") long userId);
 
-    @Query("SELECT o FROM OnboardingUserConfig o WHERE o.user.id =:userId and o.elementParent is null ")
+//    @Query("SELECT o FROM OnboardingUserConfig o WHERE o.user.id =:userId and o.elementParent is null ")
+    @Query("SELECT o FROM OnboardingUserConfig o WHERE o.user.id =:userId ")
     List<OnboardingUserConfig> findElementParentsByUserId(@Param("userId") long userId);
 
     @Query("SELECT o FROM OnboardingUserConfig o WHERE o.user.id =:userId and o.elementParent = :elementParent")
     List<OnboardingUserConfig> findElementChildsByUserId(@Param("userId") long userId, @Param("elementParent") String elementParent);
 
 
-	@Query("SELECT ob from OnboardingUserConfig ob where ob.userId = :userId and ob.active = 'Y' and elementType = 'BRANCH' order by ob.element")
+	@Query("SELECT ob from OnboardingUserConfig ob where ob.user.id = :userId and ob.active = 'Y' and elementType = 'BRANCH' order by ob.element")
 	List<OnboardingUserConfig> findbranchListByUserId(@Param("userId") long userId);
 	
-	@Query("SELECT ob from OnboardingUserConfig ob where ob.userId = :userId and ob.active = 'Y' and elementType = 'PROJECT' order by ob.element")
+	@Query("SELECT ob from OnboardingUserConfig ob where ob.user.id = :userId and ob.active = 'Y' and elementType = 'PROJECT' order by ob.element")
 	List<OnboardingUserConfig> findProjectByBranchId(@Param("userId") long userId);
 	
-	@Query("SELECT ob from OnboardingUserConfig ob where ob.userId = :userId and ob.active = 'Y' and elementType = 'WBS' order by ob.element")
+	@Query("SELECT ob from OnboardingUserConfig ob where ob.user.id = :userId and ob.active = 'Y' and elementType = 'WBS' order by ob.element")
 	List<OnboardingUserConfig> findWBSByProjectId(@Param("userId") long userId);
 }
