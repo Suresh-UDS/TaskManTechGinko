57fc6e9aa2300157a088bd33ec1ba1cb361a76ee
Ramnivas Murugesan <ramnivas.m@uds.in>
2019-08-28 14:58:30 +0530

    import firstname lastname operation

--- Webadmin/src/main/webapp/scripts/app/admin/onBoarding/onBoarding.controller.js
+++ Webadmin/src/main/webapp/scripts/app/admin/onBoarding/onBoarding.controller.js
@@ -908,7 +908,9 @@ angular.module('timeSheetApp')
                                 $scope.employee.religion !=null &&
                                 $scope.employee.wbsDescription !=null &&
                                 $scope.employee.wbsId !=null &&
-                                documents.length>8
+								_.where(documents,{docType:'adhar_card_front'}) &&
+								_.where(documents,{docType:'adhar_card_back'}) && 
+								employee.newEmployee &&  _.where(documents,{docType:'bank_passbook_image'}) &&
 
                             ){
                                 $scope.enableApproval = true;
